1c15978b59365ce920ddcaf5f1926c9ba792de2f
Hyeonah Jung <gwi02378@gmail.com>
2021-05-12 23:23:39 +0900

    binding activity_garden

---
+++ app/src/main/java/com/example/sunflower_clone/GardenActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sunflower_clone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil.setContentView
+import com.example.sunflower_clone.databinding.ActivityGardenBinding
+
+class GardenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView<ActivityGardenBinding>(this, R.layout.activity_garden)
+    }
+}
