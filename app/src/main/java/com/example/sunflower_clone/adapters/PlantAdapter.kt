b1fb4b0ca49439482acaa35f7dc1e6f3f6627e04
Hyeonah Jung <gwi02378@gmail.com>
2021-06-01 23:06:20 +0900

    PlantAdapter add diffCallback

--- app/src/main/java/com/example/sunflower_clone/adapters/PlantAdapter.kt
+++ app/src/main/java/com/example/sunflower_clone/adapters/PlantAdapter.kt
@@ -1,11 +1,22 @@
 package com.example.sunflower_clone.adapters
 
 import android.widget.ListAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sunflower_clone.data.Plant
 
 class PlantAdapter : ListAdapter<Plant, RecyclerView.ViewHolder>() {
     class PlantViewHolder() : RecyclerView.ViewHolder(binding.root) {
 
+    }
+}
+
+private class PlantDiffCallback : DiffUtil.ItemCallback<Plant>() {
+    override fun areItemsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+        return oldItem.plantId == newItem.plantId
+    }
+
+    override fun areContentsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+        return oldItem == newItem
     }
 }
