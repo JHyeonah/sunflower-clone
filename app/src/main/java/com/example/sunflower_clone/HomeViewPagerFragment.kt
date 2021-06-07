4ab6917e766942b81bba14830e4ab6ea4f514f75
Hyeonah Jung <gwi02378@gmail.com>
2021-06-07 22:53:43 +0900

    HomeViewPagerFragment.kt 완료

--- app/src/main/java/com/example/sunflower_clone/HomeViewPagerFragment.kt
+++ app/src/main/java/com/example/sunflower_clone/HomeViewPagerFragment.kt
@@ -6,9 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.sunflower_clone.adapters.MY_GARDEN_PAGE_INDEX
+import com.example.sunflower_clone.adapters.PLANT_LIST_PAGE_INDEX
+import com.example.sunflower_clone.adapters.SunflowerPagerAdapter
 import com.example.sunflower_clone.databinding.FragmentViewPagerBinding
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
+import java.lang.IndexOutOfBoundsException
 
 @AndroidEntryPoint
 class HomeViewPagerFragment : Fragment() {
@@ -17,9 +21,12 @@ class HomeViewPagerFragment : Fragment() {
         val tabLayout = binding.tabs
         val viewPager = binding.viewPager
 
+        viewPager.adapter = SunflowerPagerAdapter(this)
+
         // set icon and text for tabs
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-
+            tab.setIcon(getTabIcon(position))
+            tab.text = getTabTitle(position)
         }.attach()
 
         (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
@@ -29,7 +36,17 @@ class HomeViewPagerFragment : Fragment() {
 
     private fun getTabIcon(position: Int): Int {
         return when (position) {
-            0
+            MY_GARDEN_PAGE_INDEX -> R.drawable.garden_tab_selector
+            PLANT_LIST_PAGE_INDEX -> R.drawable.plant_list_tab_selector
+            else -> throw IndexOutOfBoundsException()
+        }
+    }
+
+    private fun getTabTitle(position: Int): String? {
+        return when (position) {
+            MY_GARDEN_PAGE_INDEX -> getString(R.string.my_garden_title)
+            PLANT_LIST_PAGE_INDEX -> getString(R.string.plant_list_title)
+            else -> null
         }
     }
 }
