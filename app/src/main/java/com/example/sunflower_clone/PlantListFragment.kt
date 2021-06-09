93a3cf9297a33d89cf4112c3295b1494805ec45d
Hyeonah Jung <gwi02378@gmail.com>
2021-06-09 22:46:32 +0900

    add menu_plant_list.xml

--- app/src/main/java/com/example/sunflower_clone/PlantListFragment.kt
+++ app/src/main/java/com/example/sunflower_clone/PlantListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.sunflower_clone.adapters.PlantAdapter
 import com.example.sunflower_clone.databinding.FragmentPlantListBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,7 +15,13 @@ class PlantListFragment : Fragment() {
         val binding = FragmentPlantListBinding.inflate(inflater, container, false)
         context ?: return binding.root
 
-
+        val adapter = PlantAdapter()
+        binding.plantList.adapter = adapter
+        subscribeUi(adapter)
         return binding.root
+    }
+
+    private fun subscribeUi(adapter: PlantAdapter) {
+
     }
 }
