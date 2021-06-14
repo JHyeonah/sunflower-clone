2b8126bbfc697d71a7efd1df167e03b4ce345a47
Hyeonah Jung <gwi02378@gmail.com>
2021-06-14 22:44:26 +0900

    PlantDao.kt 수정

--- app/src/main/java/com/example/sunflower_clone/data/PlantDao.kt
+++ app/src/main/java/com/example/sunflower_clone/data/PlantDao.kt
@@ -1,6 +1,8 @@
 package com.example.sunflower_clone.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
@@ -8,4 +10,13 @@ import kotlinx.coroutines.flow.Flow
 interface PlantDao {
     @Query("SELECT * FROM plants ORDER BY name")
     fun getPlants(): Flow<List<Plant>>
+
+    @Query("SELECT * FROM plants WHERE growZoneNumber = :growZoneNumber ORDER BY name")
+    fun getPlantsWithGrowZoneNumber(growZoneNumber: Int): Flow<List<Plant>>
+
+    @Query("SELECT * FROM plants WHERE id = :plantId")
+    fun getPlant(plantId: String): Flow<Plant>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(plant: List<Plant>)
 }
