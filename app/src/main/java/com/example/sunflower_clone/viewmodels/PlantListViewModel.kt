033f521e16498b7f1109fff641016f29d165439f
Hyeonah Jung <gwi02378@gmail.com>
2021-06-16 22:57:22 +0900

    add PlantListViewModel.kt

--- app/src/main/java/com/example/sunflower_clone/viewmodels/PlantListViewModel.kt
+++ app/src/main/java/com/example/sunflower_clone/viewmodels/PlantListViewModel.kt
@@ -1,4 +1,23 @@
 package com.example.sunflower_clone.viewmodels
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.sunflower_clone.data.PlantRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
 
+@HiltViewModel
+class PlantListViewModel @Inject internal constructor(
+        plantRepository: PlantRepository,
+        private val savedStateHandle: SavedStateHandle
+): ViewModel() {
+
+    private val growZone: MutableStateFlow<Int> = MutableStateFlow(savedStateHandle.get(GROW_ZONE_SAVED_STATE_KEY) ?: NO_GROW_ZONE)
+
+
+    companion object {
+        private const val NO_GROW_ZONE = -1
+        private const val GROW_ZONE_SAVED_STATE_KEY = "GROW_ZONE_SAVED_STATE_KEY"
+    }
+}
