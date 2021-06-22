aa24de554bdb570b30eb40fa558beab597bb1860
Hyeonah Jung <gwi02378@gmail.com>
2021-06-22 23:02:39 +0900

    PlantListViewModel.kt 완료

--- app/src/main/java/com/example/sunflower_clone/viewmodels/PlantListViewModel.kt
+++ app/src/main/java/com/example/sunflower_clone/viewmodels/PlantListViewModel.kt
@@ -1,13 +1,14 @@
 package com.example.sunflower_clone.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.example.sunflower_clone.data.Plant
 import com.example.sunflower_clone.data.PlantRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,6 +19,21 @@ class PlantListViewModel @Inject internal constructor(
 
     private val growZone: MutableStateFlow<Int> = MutableStateFlow(savedStateHandle.get(GROW_ZONE_SAVED_STATE_KEY) ?: NO_GROW_ZONE)
 
+    val plants: LiveData<List<Plant>> = growZone.flatMapLatest { zone ->
+        if (zone == NO_GROW_ZONE) {
+            plantRepository.getPlants()
+        } else {
+            plantRepository.getPlantsWithGrowZoneNumber(zone)
+        }
+    }.asLiveData()
+
+    init {
+        viewModelScope.launch {
+            growZone.onEach { newGrowZone ->
+                savedStateHandle.set(GROW_ZONE_SAVED_STATE_KEY, newGrowZone)
+            }
+        }.collect()
+    }
 
     fun setGrowZoneNumber(num: Int) {
         growZone.value = num
