69278fad03e9f2003717ed51444ab19d1f2d5739
Hyeonah Jung <gwi02378@gmail.com>
2021-07-06 22:16:17 +0900

    database module 추가

--- app/src/main/java/com/example/sunflower_clone/di/DatabaseModule.kt
+++ app/src/main/java/com/example/sunflower_clone/di/DatabaseModule.kt
@@ -1,13 +1,26 @@
 package com.example.sunflower_clone.di
 
+import android.content.Context
+import com.example.sunflower_clone.data.AppDatabase
+import com.example.sunflower_clone.data.PlantDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class DatabaseModule {
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return AppDatabase.getInstance(context)
+    }
 
+    @Provides
+    fun providePlantDao(database: AppDatabase): PlantDao {
+        return database.plantDao()
+    }
 }
