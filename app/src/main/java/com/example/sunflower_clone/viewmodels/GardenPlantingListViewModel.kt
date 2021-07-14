be76f861b1c75fccf4a650e6456d933d0e9539ae
Hyeonah Jung <gwi02378@gmail.com>
2021-07-14 22:55:21 +0900

    binding to garden fragment, applying adapter

--- app/src/main/java/com/example/sunflower_clone/viewmodels/GardenPlantingListViewModel.kt
+++ app/src/main/java/com/example/sunflower_clone/viewmodels/GardenPlantingListViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.sunflower_clone.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.example.sunflower_clone.data.GardenPlantingRepository
+import com.example.sunflower_clone.data.PlantAndGardenPlantings
 import javax.inject.Inject
 
-class GardenPlantingListViewModel @Inject internal constructor() : ViewModel() {
-
+class GardenPlantingListViewModel @Inject internal constructor(gardenPlantingRepository: GardenPlantingRepository) : ViewModel() {
+    val plantAndGardenPlantings: LiveData<List<PlantAndGardenPlantings>> = gardenPlantingRepository.getPlantedGardens().asLiveData()
 }
