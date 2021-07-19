fba6f1c603f0732df7d7e153cfce428abaa37595
Hyeonah Jung <gwi02378@gmail.com>
2021-07-19 21:39:23 +0900

    navigation 연결하여 메인화면 표시 완료

--- app/src/main/java/com/example/sunflower_clone/viewmodels/GardenPlantingListViewModel.kt
+++ app/src/main/java/com/example/sunflower_clone/viewmodels/GardenPlantingListViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.example.sunflower_clone.data.GardenPlantingRepository
 import com.example.sunflower_clone.data.PlantAndGardenPlantings
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class GardenPlantingListViewModel @Inject internal constructor(gardenPlantingRepository: GardenPlantingRepository) : ViewModel() {
     val plantAndGardenPlantings: LiveData<List<PlantAndGardenPlantings>> = gardenPlantingRepository.getPlantedGardens().asLiveData()
 }
