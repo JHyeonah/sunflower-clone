8e62ab6a3b74d95af8bddfbeb6e46829e9a3a41b
Hyeonah Jung <gwi02378@gmail.com>
2021-07-26 21:55:30 +0900

    PlantDetailViewModel 완료, unsplash build config 추가

--- app/src/main/java/com/example/sunflower_clone/viewmodels/PlantDetailViewModel.kt
+++ app/src/main/java/com/example/sunflower_clone/viewmodels/PlantDetailViewModel.kt
@@ -2,9 +2,13 @@ package com.example.sunflower_clone.viewmodels
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.sunflower_clone.BuildConfig
 import com.example.sunflower_clone.data.GardenPlantingRepository
 import com.example.sunflower_clone.data.PlantRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,6 +19,16 @@ class PlantDetailViewModel @Inject constructor(
 ) : ViewModel() {
 
     val plantId: String = savedStateHandle.get<String>(PLANT_ID_STAVED_STATE_KEY)!!
+    val isPlanted = gardenPlantingRepository.isPlanted(plantId).asLiveData()
+    val plant = plantRepository.getPlant(plantId).asLiveData()
+
+    fun addPlantToGarden() {
+        viewModelScope.launch {
+            gardenPlantingRepository.createGardenPlanting(plantId)
+        }
+    }
+
+    fun hasValidUnsplashKey() = (BuildConfig.UNSPLASH_ACCESS_KEY != "null")
 
     companion object {
         private const val PLANT_ID_STAVED_STATE_KEY = "plantId"
