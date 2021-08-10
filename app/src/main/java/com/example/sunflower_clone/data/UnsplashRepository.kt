afda982dc28d073ddbe9b90b74f0eccc232e2b8c
Hyeonah Jung <gwi02378@gmail.com>
2021-08-10 22:16:55 +0900

    create GalleryViewModel, UnsplashRepository

---
+++ app/src/main/java/com/example/sunflower_clone/data/UnsplashRepository.kt
@@ -0,0 +1,23 @@
+package com.example.sunflower_clone.data
+
+import android.app.DownloadManager
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.sunflower_clone.api.UnsplashService
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class UnsplashRepository @Inject constructor(private val service: UnsplashService) {
+
+    fun getSearchResultStream(query: String): Flow<PagingData<UnsplashPhoto>> {
+        return Pager(
+            config = PagingConfig(enablePlaceholders = false, pageSize = NETWORK_PAGE_SIZE),
+                pagingSourceFactory = { UnsplashPagingSource(service, query) }
+        ).flow
+    }
+
+    companion object {
+        private const val NETWORK_PAGE_SIZE = 25
+    }
+}
